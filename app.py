0d34c1a64b9c3c54c73e80a6fb235d8ff5bf203a
Lalith Reddy <144982183+Lalith2907@users.noreply.github.com>
2024-07-15 17:42:45 +0530

    added 'Switch Theme' feature

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import requests
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
+
+app.secret_key = 'your_key'
 
 API_KEY = 'your_api_key'
 BASE_URL = 'https://api.openweathermap.org/data/2.5/'
@@ -43,13 +45,22 @@ def weather():
 
                 return render_template('weather.html', current=weather_data, temp_c=temp_c, temp_f=temp_f, forecast_list=forecast_list)
             else:
-                error_message = 'Failed to fetch forecast data. Please try again later.'
+                error_message = 'Failed to fetch forecast data. Please try again.'
         else:
-            error_message = 'Failed to fetch weather data. Please try again later.'
+            error_message = 'Failed to fetch weather data. Please try again.'
         
         return render_template('layout.html', error=error_message)
     
     return render_template('layout.html', error='Please enter a city.')
+
+@app.route('/toggle-theme', methods=['POST'])
+def toggle_theme():
+    if 'theme' in session:
+        session['theme'] = 'dark' if session['theme'] == 'light' else 'light'
+    else:
+        session['theme'] = 'light'
+    
+    return redirect(request.referrer or '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
